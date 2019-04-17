de6db8c5d31633c4ec0ff6d3e09fc9d790064b4b
Kyile <kyile.woods@connect.qut.edu.au>
2019-04-18 00:50:34 +1000

    Still starting vid1

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-subst w: C:\Users\kwood178\Desktop\"Topics& Projects"\Programming\C\HandMadeHero
+subst w: C:\Users\kwood178\Desktop\"Topics& Projects"\Programming\C\handmade
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall"
-set  path=w:\handmade\misc;%path%
+set  path=w:\misc;%path%
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,4 @@
 @echo off
-subst w: C:\Users\kwood178\Desktop\"Topics& Projects"\Programming\C\HandMadeHero
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall"
-set  path=w:\handmade\misc;%path%
+subst w: C:\Users\kwood178\Desktop\"Topics& Projects"\Programming\C\handmade
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set  path=w:\misc;%path%
